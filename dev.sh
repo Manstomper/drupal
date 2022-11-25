f921030b91360d695637ce0d9a36e3c2c66f5ec2
Manstomper <nospam@nospam.com>
2022-11-25 10:20:17 +0200

    small improvements and fixes

--- dev.sh
+++ dev.sh
@@ -2,13 +2,16 @@
 
 if [[ $(docker compose ps -q php 2>&1) =~ "no such service" ]]; then
     echo "Container does not appear to exist. Building and starting..."
+
     cp -n ./.env.local ./.env
 
     docker build . -f "$(pwd)/dev/nginx/Dockerfile" -t rig/nginx:1.23.2 \
         && docker build . -f "$(pwd)/dev/php/Dockerfile" -t rig/php:8.1.12 \
         && docker compose up -d \
-        && docker compose run --rm -v "$(pwd):/app-mount" -w /app-mount php sh -c "composer install --no-interaction"
+        && docker compose run --rm -v "$(pwd):/app-mount" -w /app-mount php sh -c \
+            "composer install --no-interaction && ln -s /app-mount/public/vendor/bin/drush /bin/drush"
 else
     echo "Container appears to exist. Restarting..."
+
     docker compose restart
 fi
