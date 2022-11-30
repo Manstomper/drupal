9b52417f170a5a8c42e8ccadb528cbe37d27623b
Manstomper <nospam@nospam.com>
2022-11-30 13:24:14 +0200

    moved docker build script to file, composer update

---
+++ dev/rebuild.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker build . -f "$(pwd)/dev/php/Dockerfile" -t rig/php:8.1.12 \
+    && docker compose up -d \
+    && docker compose exec php sh -c \
+        "cd /app-mount && composer install --no-interaction && ln -s /app-mount/public/vendor/bin/drush /bin/drush"
