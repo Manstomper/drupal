a21e1ff53f009411a5996ae2f72c406321311597
Manstomper <nospam@nospam.com>
2022-12-22 10:35:43 +0200

    restructured

--- .local/rebuild.sh
+++ .local/rebuild.sh
@@ -2,7 +2,9 @@
 
 cp -n ./.env.local ./.env
 
-docker build . -f "$(pwd)/.local/php/Dockerfile" -t rig/php:8.1.12 \
+docker build . -f "$(pwd)/.local/php/Dockerfile" -t drupal/php:8.1.12 \
     && docker compose up -d \
-    && docker compose exec php sh -c \
-        "cd /app-mount && composer install --no-interaction && ln -s /app-mount/vendor/bin/drush /bin/drush"
+    && docker compose exec php sh -c "cd /app-mount && composer install --no-interaction" \
+    && cp -n ./public/sites/example.settings.local.php ./public/sites/default/settings.local.php \
+    && cp -n ./.local/drupal/development.services.yml ./public/sites/ \
+    && printf '\nif (file_exists(\"{$app_root}/{$site_path}/settings.local.php\")) { include \"{$app_root}/{$site_path}/settings.local.php\"; }\n\n' >> ./public/sites/default/default.settings.php
