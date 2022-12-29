8546b952ac0c617b387f0b89e80d60fa4b038c14
Manstomper <nospam@nospam.com>
2022-12-29 14:37:44 +0200

    really disable cache, added vs code settings

--- .local/rebuild.sh
+++ .local/rebuild.sh
@@ -4,7 +4,11 @@ cp -n ./.local/.env.local ./.env
 
 docker build . -f "$(pwd)/.local/php/Dockerfile" -t drupal/php:8.1.12 \
     && docker compose up -d \
-    && docker compose exec -w /app-mount php sh -c "composer install --no-interaction" \
-    && cp -n ./public/sites/example.settings.local.php ./public/sites/default/settings.local.php \
-    && cp -n ./.local/drupal/development.services.yml ./public/sites/ \
-    && printf '\nif (file_exists(\"{$app_root}/{$site_path}/settings.local.php\")) { include \"{$app_root}/{$site_path}/settings.local.php\"; }\n\n' >> ./public/sites/default/default.settings.php
+    && docker compose exec -w /app-mount php sh -c "composer install --no-interaction"
+
+printf 'if (file_exists(\"{$app_root}/{$site_path}/settings.local.php\")) { include \"{$app_root}/{$site_path}/settings.local.php\"; }\n\n' >> ./public/sites/default/default.settings.php
+
+cp -n ./.local/drupal/development.services.yml ./public/sites/
+
+echo "\$settings['cache']['bins']['render']='cache.backend.null';\$settings['cache']['bins']['discovery_migration']='cache.backend.memory';\$settings['cache']['bins']['dynamic_page_cache']='cache.backend.null';" >> ./public/sites/example.settings.local.php
+cp -n ./public/sites/example.settings.local.php ./public/sites/default/settings.local.php
